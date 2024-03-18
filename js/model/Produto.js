4f012eaca74ee8a37e77fd8c4656d7f4028cc13e
leonardopontes91 <163802559+leonardopontes91@users.noreply.github.com>
2024-03-18 00:19:36 -0300

    Add files via upload

---
+++ js/model/Produto.js
@@ -0,0 +1,17 @@
+class Produto {
+    constructor(obj){
+        obj = obj || {}// Isso é um tratamento para evitar undefined quando tentar acessar alguma propriedade.
+        this.id = obj.id;
+        this.nome = obj.nome;
+        this.valor = obj.valor;
+        this.quantidadeEstoque = obj.quantidadeEstoque;
+        this.observacao = obj.observacao;
+        this.foto = obj.foto;
+        this.dataCadastro = obj.dataCadastro;
+    }
+
+    validar(){
+        
+        return !!(this.nome && this.valor);  
+    }
+}
